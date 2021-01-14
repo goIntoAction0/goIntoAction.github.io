c95a67324d914703f23bed308fbdb0c52e400bd3
肖志星 <zhixing_xiao@163.com>
2021-01-14 23:23:54 +0800

    导航栏、侧边栏、最后更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,29 @@
 module.exports = {
-  base: 'xinBlog',
+  base: '/xinBlog/',
   title: '阿星的个人博客！',
-  description: 'Just playing around',
+  description: '描述：nice！！！',
   // theme: 'xxx',
   themeConfig: {
     logo: '/assets/img/logo.png',                    // 导航栏logo
+    lastUpdated: '最后更新时间',                     // 最后更新时间:  string | boolean
     nav: [                                           // 导航栏链接
-      { text: 'HTML', link: '/html/' },
+      { 
+        text: '前端工具', 
+        // ariaLabel: '前端工具 Menu',
+        items: [
+          { text: 'GIT', link: '/tools/git.html' },
+          { text: 'NPM', link: '/tools/npm.html' }
+        ]
+      },
+      { text: 'HTML', link: '/html/html-1.html' },
       { text: 'CSS', link: '/css/css-1.html' },
-      { text: 'JavaScript', link: '/js/' },
-      { text: 'Vue', link: '/vue/html-1.html' },
-      { text: '前端工具', link: '/vue/' },
+      { text: 'JavaScript', link: '/js/js-1.html' },
+      { text: 'Vue', link: '/vue/vue-1.html' },
       { text: 'DOM', link: '/' },
     ],
     sidebar: 'auto',                                // 自动生成侧栏 值：可以是一个对象或者数组；可以有一个或多个侧边栏
     displayAllHeaders: true,                        // 显示所有页面的标题链接   默认值：false
     activeHeaderLinks: false,                       // 活动的标题链接  默认值：true
-  }
+  },
+  
 }
