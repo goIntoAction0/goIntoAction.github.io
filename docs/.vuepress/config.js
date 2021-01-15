af59e6107c1ac8dbbb1465e78f31026de1b2d0f2
肖志星 <zhixing_xiao@163.com>
2021-01-16 01:11:31 +0800

    加了一点代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,26 +2,32 @@ module.exports = {
   base: '/xinBlog/',
   title: '阿星的个人博客！',
   description: '描述：nice！！！',
+  searchMaxSuggestions: 10,                         // 搜索结果数量
 
 
 //  主题配置
   themeConfig: {
     logo: '/assets/img/logo.png',                    // 导航栏logo
-    lastUpdated: '最后更新时间',                       // 最后更新时间:  string | boolean
+    lastUpdated: '更新时间',                          // 最后更新时间:  string | boolean
     
     nav: [                                           // 导航栏链接
       { 
         text: '前端工具', 
         // ariaLabel: '前端工具 Menu',
         items: [
-          { text: 'GIT', link: '/tools/git.html' },
-          { text: 'NPM', link: '/tools/npm.html' }
+          { text: 'git', link: '/tools/git.html' },
+          { text: 'github', link: '/tools/github.html' },
+          { text: 'MarkDown', link: '/tools/markdown.html' },
+          { text: 'NPM', link: '/tools/npm.html' },
         ]
       },
       { text: 'vuepress', link: '/vuepress-config/config.html' },
       { text: 'CSS', link: '/css/css-1.html' },
       { text: 'JavaScript', link: '/js/' },
       { text: 'Vue', link: '/vue/vue-1.html' },
+      { text: 'BUG-LOG', link: '/bug/bug-1.html' },
+      { text: '项目', link: '/project/project.html' },
+      // { text: '算法', link: '/project/project.html' },
     ],
 
     sidebar: 'auto',                                // 自动生成侧栏 值：可以是一个对象或者数组；可以有一个或多个侧边栏
---
+++ docs/tools/github.md
@@ -0,0 +1,26 @@
+---
+description: 'git'
+---
+
+[[toc]]
+# github
+## github搜索框的使用：
+``` sh
+# 指定使用的语言
+in:language:<javascript>
+
+# 指定stars范围(指定星数范围)
+in:stars:>100
+
+# 搜索项目自我描述中，包含的关键字
+in:readme <关键字>
+
+# 指定名称的项目
+in:name <项目名称>
+
+# 要求forks数大于一定的值：
+in:forks>100
+
+# 指定日期后更新过的
+in:pushed:2019-01-01
+```
